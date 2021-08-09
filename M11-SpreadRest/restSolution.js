55073a06c3c264bd98caa3376380f415d2f30cf9
huijielim <huijielim@gmail.com>
2021-08-09 14:41:51 +0800

    first commit

---
+++ M11-SpreadRest/restSolution.js
@@ -0,0 +1,21 @@
+// TODO: create the function sumAll
+
+//original solution which didnt work 
+// function sumAll(a, b, ...rest=0){
+//   return a+b+rest;
+// } 
+
+const sumAll = (...args) => {
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+};
+
+console.log(sumAll(1, 2)); // expected output: 3
+console.log(sumAll(1, 2, 3)); // expected output: 6
+console.log(sumAll(1, 2, 3, 4)); // expected output: 10
+
+//don't change this line
+if (typeof module !== "undefined") {
+  module.exports = { sumAll };
+}
